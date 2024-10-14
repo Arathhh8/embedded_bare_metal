859251cb1948caafadc7a847d72cd7d56a023c2b
Arathhh8 <arathgg143@gmail.com>
2024-10-13 22:32:18 -0700

    add logic to copy .data to SRAM, initialize .bss section and call main function

--- src/stm32_startup.c
+++ src/stm32_startup.c
@@ -6,6 +6,15 @@
 
 #define STACK_START     SRAM_END
 
+extern uint32_t _etext;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+// Prototype of main()
+int main(void);
+
 void Reset_Handler(void);
 void NMI_Handler(void)                          __attribute__((weak, alias("Default_Handler")));
 void HardFault_Handler(void)                    __attribute__((weak, alias("Default_Handler")));
@@ -163,8 +172,25 @@ void Default_Handler(void)
 void Reset_Handler(void)
 {
     // copy .data section to SRAM
+    uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+
+    uint8_t *pDst = (uint8_t*)&_sdata;
+    uint8_t *pSrc = (uint8_t*)&_etext;
+
+    for(uint32_t i = 0; i < size; i++)
+    {
+        *pDst++ = *pSrc++;
+    }
 
     // Init the .bss section to zero in SRAM
+    size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+    pDst = (uint8_t*)&_sbss;
+
+    for(uint32_t i = 0; i < size; i++)
+    {
+        *pDst++ = 0;
+    }
 
     // call main()
+    main();
 }
